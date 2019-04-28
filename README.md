c2329faa2184437bf2070ab707beae9523897326
Pascal ECHEMANN <pascal@wooz.io>
2019-04-28 20:38:12 +0200

    Hyperion update
    Add an hyperion sample file.

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Asteria is a specification for building data analytics modules that are executed
 | Crios | `asteria-crios` | Crios provides the Asteria modules for the Ouranos implementation. |
 | Cronos | `asteria-cronos` | Cronos provides a set of Asteria modules that can be deployed within a Node.js environment. |
 | Atlas | `asteria-atlas` | Atlas provides a set of Asteria modules that can be used by Angular applications. |
+| Hyperion | `asteria-hyperion` | Hyperion provides the API to create Asteria process definitions in JSON format. The Hyperion files can be used to share any Asteria process over distributed systems. |
 
 ## Assets
 
---
+++ src/index.2.ts
@@ -0,0 +1,50 @@
+import * as path from 'path';
+
+const buildFilePath: Function = (fileName: string)=> { 
+    return path.join(__dirname, 'temp-data', fileName);
+};
+
+const hyperionSample: any /*HyperionConfig*/ = {
+    name: 'Asteria Sample Process',
+    processor: {
+        processList: [
+            {
+                name: 'FileReaderModule',
+                input: buildFilePath('worldcitiespop.csv')
+            },
+            {
+                name: 'CsvToListModule',
+                config: {
+                    trimFirstRow: true,
+                    separator: ';',
+                    colsMapping: [
+                        { index: 0, property: 'country' },
+                        { index: 2, property: 'city' },
+                        { index: 3, property: 'region' },
+                        { index: 4, property: 'population', castFuncRef: 'number' },
+                        { index: 5, property: 'Latitude' },
+                        { index: 6, property: 'longitude' }
+                    ]
+                }
+            },
+            {
+                name: 'FilterListModuleConfig',
+                config: {
+                    condition: 'AND',
+                    filters: [
+                        {
+                            property: 'population',
+                            operator: 'GREATER_THAN',
+                            value: 1000000
+                        },
+                        {
+                            property: 'country',
+                            operator: 'LIKE',
+                            value: 'us'
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+};
